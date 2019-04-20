746cf7688f9520b00e662b5f8a12968288565f10
Tito <tito.parra4@@hotmail.com>
2019-04-20 09:13:51 +0200

    First commit

---
+++ precache-manifest.504cca8b7b74892545546d539b3d7fc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f007d52148e2b8644108",
+    "url": "/static/js/main.92c350da.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "f007d52148e2b8644108",
+    "url": "/static/css/main.f35296ef.chunk.css"
+  },
+  {
+    "revision": "4909e9fd539b809866017da69a232e3e",
+    "url": "/index.html"
+  }
+];
